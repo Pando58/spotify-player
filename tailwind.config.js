92b42ea608818f67d0c7b97e982966b6a7e0bafc
Enrique Pando <enr.pando@gmail.com>
2023-02-28 13:21:33 -0600

    Spotify logo and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
         "zinc-850": "rgb(32, 32, 34)",
         "zinc-750": "rgb(51, 51, 56)",
         "zinc-650": "rgb(73, 73, 81)",
+        "spotify-green": "#1ED760",
+        "spotify-green-cmyk": "#1AB26B",
       },
     },
   },
