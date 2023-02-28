0faf1d2f2cf73f99fa382e369c30ddc13ba31bca
Enrique Pando <enr.pando@gmail.com>
2023-02-27 18:37:07 -0600

    Tailwind zinc color variations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
       fontFamily: {
         sans: ["var(--inter-font)"],
       },
+      colors: {
+        "zinc-950": "rgb(12, 12, 13)",
+        "zinc-850": "rgb(32, 32, 34)",
+        "zinc-750": "rgb(51, 51, 56)",
+        "zinc-650": "rgb(73, 73, 81)",
+      },
     },
   },
   plugins: [],
