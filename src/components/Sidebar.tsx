a0978db71005195f4795c5735c303c26761350d5
Enrique Pando <enr.pando@gmail.com>
2023-02-28 22:15:45 -0600

    Sidebar user section

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,8 @@
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { HeartIcon, HomeIcon, MagnifyingGlassIcon, PlusIcon, QueueListIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 import { useMemo } from "react";
+import userImage from "@/../public/profile.jpg";
 
 export function Sidebar({ bottomSpace }: { bottomSpace: number }) {
   const entries = useMemo(() => ([
@@ -32,14 +35,22 @@ export function Sidebar({ bottomSpace }: { bottomSpace: number }) {
   ]), []);
 
   return (
-    <div className="h-full w-48 overflow-y-auto border-r border-zinc-800 bg-zinc-950 py-2">
+    <nav className="h-full w-48 overflow-y-auto border-r border-zinc-800 bg-zinc-950 py-2 text-xs font-medium text-zinc-400 sm:w-56 sm:text-sm">
+      <button className="mt-2 mb-4 flex w-full items-center gap-3 px-4 duration-75 hover:text-zinc-100 sm:gap-4">
+        <div className="h-6 w-6">
+          <Image src={userImage} className="rounded-full" alt="user image" />
+        </div>
+        <span>John Doe</span>
+        <div className="flex-1" />
+        <ChevronDownIcon className="w-4" />
+      </button>
       <ul>
         {entries.map(({ icon, text, separator }, i) => (
           <li key={i}>
             {separator ? (
               <hr className="my-3 mx-4 border-zinc-750" />
             ) : (
-              <button className="flex w-full items-center gap-2 py-2 px-4 text-xs font-medium text-zinc-400 duration-75 hover:text-zinc-100 sm:text-sm">
+              <button className="flex w-full items-center gap-2 py-2 px-4 duration-75 hover:text-zinc-100">
                 <div className="w-4 sm:w-5">{icon}</div>
                 <span>{text}</span>
               </button>
@@ -60,6 +71,6 @@ export function Sidebar({ bottomSpace }: { bottomSpace: number }) {
           height: bottomSpace + "rem",
         }}
       />
-    </div>
+    </nav>
   );
 }
